8fef63f45be62ee0c3fe7e43c1ada9ae553880dd
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2020-06-27 17:50:43 +0200

    organisation API: fix HTTP PUT and add HTTP PATCH
    Motivation:
    
    By standard definition, the HTTP PUT request method creates
    a new resource or replaces a representation of the target
    resource with the request payload. Also, PUT is idempotent
    whereas POST is not.
    
    Currently, the API implementation treat both PUT and POST
    in the same way. Hence, cerebrum is not following proper
    restful api protocol definition.
    
    Modification:
    
    - reimplement the PUT http method to create a new resources or
        replace an existing one.
    - add PATCH http method to partially update an existing resouces.
        This is achieve by using `json-patch` to describe the changes
        in the request body, hence the patch http method will consume
        `application/json-patch+json` (see http://jsonpatch.com/ on
        what json-patch is all about and how to use it).
    
    Result:
    
    - PUT request either create a new entry or replace an extisting
        one
    - PATCH method can be use to partially update exitsing resources.
    
    Target: master
    Acked-by: Franz Stephan
    Merge-request: https://gitlab.hzdr.de/hifis-technical-platform/helmholtz-cerebrum/-/merge_requests/6

--- src/main/java/de/helmholtz/marketplace/cerebrum/controller/OrganizationController.java
+++ src/main/java/de/helmholtz/marketplace/cerebrum/controller/OrganizationController.java
@@ -1,6 +1,7 @@
 package de.helmholtz.marketplace.cerebrum.controller;
 
 import de.helmholtz.marketplace.cerebrum.entities.Organization;
+import de.helmholtz.marketplace.cerebrum.exception.OrganizationNotFoundException;
 import de.helmholtz.marketplace.cerebrum.repository.OrganizationRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,11 +13,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.lang.Nullable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -101,12 +110,61 @@ public class OrganizationController {
             @ApiResponse(responseCode = "200", description = "successful operation"),
             @ApiResponse(responseCode = "400", description = "invalid ID supplied")
     })
-    @PutMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Organization updateOrganization(
-            @Parameter(description="Organization to update. This cannot be null or empty.",
-            required=true, schema=@Schema(implementation = Organization.class))
-                                               @Valid @RequestBody Organization organization) {
-        return organizationRepository.save(organization);
+            @Parameter(
+                    description="Organization to update or replace. This cannot be null or empty.",
+                    schema=@Schema(implementation = Organization.class),
+                    required=true) @Valid @RequestBody Organization newOrganization,
+            @Parameter(description = "ID of the organisation that needs to be updated")
+            @PathVariable(required = true) Long id)
+    {
+        return organizationRepository.findById(id)
+                .map(organisation -> {
+                    organisation.setAbbreviation(newOrganization.getAbbreviation());
+                    organisation.setName(newOrganization.getName());
+                    organisation.setImg(newOrganization.getImg());
+                    organisation.setUrl(newOrganization.getUrl());
+                    return organizationRepository.save(organisation);
+                })
+                .orElseGet(() -> {
+                    newOrganization.setId(id);
+                    return organizationRepository.save(newOrganization);
+                });
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @Operation(summary = "partially update an existing organisation",
+            security = @SecurityRequirement(name = "hdf-aai"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation"),
+            @ApiResponse(responseCode = "400", description = "invalid id or json patch body"),
+            @ApiResponse(responseCode = "404", description = "organisation not found")
+    })
+    @PatchMapping(path = "/{id}", consumes = "application/json-patch+json")
+    public Organization partialUpdateOrganisation(
+            @Parameter(description="JSON Patch document structured as a JSON " +
+                    "array of objects where each object contains one of the six " +
+                    "JSON Patch operations: add, remove, replace, move, copy, and test",
+                    schema=@Schema(implementation = JsonPatch.class),
+                    required=true) @Valid @RequestBody JsonPatch patch,
+            @Parameter(description = "ID of the organisation that needs to be partially updated")
+            @PathVariable(required = true) Long id)
+    {
+        return organizationRepository.findById(id)
+                .map(organisation -> {
+                    try {
+                        Organization organizationPatched = applyPatchToOrganization(patch, organisation);
+                        return organizationRepository.save(organizationPatched);
+                    } catch (JsonPatchException e) {
+                        throw new ResponseStatusException(
+                                HttpStatus.BAD_REQUEST, "invalid id or json patch body", e);
+                    } catch (JsonProcessingException e) {
+                        throw new ResponseStatusException(
+                                HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error", e);
+                    }
+                })
+                .orElseThrow(OrganizationNotFoundException::new);
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -124,5 +182,14 @@ public class OrganizationController {
             @Parameter(description="organisation id to delete", required=true)
             @PathVariable(name = "id", required = true) Long id) {
         organizationRepository.deleteById(id);
+    }
+
+    private Organization applyPatchToOrganization(
+            JsonPatch patch,
+            Organization targetOrganization) throws JsonPatchException, JsonProcessingException
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode patched = patch.apply(objectMapper.convertValue(targetOrganization, JsonNode.class));
+        return objectMapper.treeToValue(patched, Organization.class);
     }
 }
---
+++ src/main/java/de/helmholtz/marketplace/cerebrum/exception/OrganizationNotFoundException.java
@@ -0,0 +1,4 @@
+package de.helmholtz.marketplace.cerebrum.exception;
+
+public class OrganizationNotFoundException extends RuntimeException {
+}
